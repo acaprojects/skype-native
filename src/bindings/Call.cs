2267ef3d2f1a54042776f97dba637c11f2f522b0
Kim Burgess <kim@acaprojects.com>
2017-05-16 05:55:47 +1000

    Add binding for call instantiation

---
+++ src/bindings/Call.cs
@@ -0,0 +1,52 @@
+﻿using System.Threading.Tasks;
+using System.Collections.Generic;
+
+using Microsoft.Lync.Model;
+using Microsoft.Lync.Model.Extensibility;
+using Microsoft.Lync.Model.Conversation;
+
+public class Startup
+{
+    public async Task<object> Invoke(dynamic options)
+    {
+        return Call(options.uri, options.fullscreen, options.display);
+    }
+
+    private static bool Call(string uri, bool fullscreen = true, int display = 0)
+    {
+        List<string> participants = new List<string>
+        {
+            uri
+        };
+
+        try
+        {
+            LyncClient.GetAutomation().BeginStartConversation(
+                AutomationModalities.Video,
+                participants,
+                null,
+                (ar) =>
+                {
+                    ConversationWindow win = LyncClient.GetAutomation().EndStartConversation(ar);
+                    if (fullscreen)
+                    {
+                        win.ShowFullScreen(display);
+                    }
+
+                    // TODO: close window on call hangup
+                    //win.Conversation.StateChanged += Conversation_StateChanged;
+                },
+                null);
+            return true;
+        }
+        catch (ClientNotFoundException)
+        {
+            return false;
+        }
+    }
+
+    private static void Conversation_StateChanged(object sender, ConversationStateChangedEventArgs e)
+    {
+        //throw new System.NotImplementedException();
+    }
+}
