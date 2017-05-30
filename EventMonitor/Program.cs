25e3950d9ea7821d4e329ca16d48d7bb5dba6b3d
Kim Burgess <kim@acaprojects.com>
2017-05-30 18:42:16 +1000

    Build skype event monitor utility app for debuging event subscriptions

---
+++ EventMonitor/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Lync.Model;
+using Microsoft.Lync.Model.Conversation;
+using System;
+
+namespace EventMonitor
+{
+    class Program
+    {
+        static EventHandler<T> PrintEvent<T>(string message)
+        {
+            return (o, e) => Console.WriteLine(message);
+        }
+
+        static void SubscribeToEvents()
+        {
+            var client = LyncClient.GetClient();
+
+            client.ConversationManager.ConversationAdded +=
+                PrintEvent<ConversationManagerEventArgs>("Conversation added");
+
+            client.ConversationManager.ConversationRemoved +=
+                PrintEvent<ConversationManagerEventArgs>("Conversation removed");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Listening for Skype client events");
+
+            SubscribeToEvents();
+
+            Console.WriteLine("Hit any key to quit...");
+            Console.ReadKey();
+        }
+    }
+}
